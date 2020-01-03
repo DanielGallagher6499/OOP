d5bd7ca060b89f0a6fa95ca417171af869fb6d76
DANIEL GALLAGHER - STUDENT <G00360986@gmit.ie>
2020-01-03 17:36:57 +0000

    Second Commit

--- OOP/src/ie/gmit/sw/Parser.java
+++ OOP/src/ie/gmit/sw/Parser.java
@@ -2,13 +2,16 @@ package ie.gmit.sw;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
-	public class Parser implements Runnable{
-		private Database db = null;
-		private String file;
-		private int k;
-		
+public class Parser implements Runnable {
+	private Database db = null;
+	private String file;
+	private int k;
+
 	public Parser(String file, int k) {
 		this.file = file;
 		this.k = k;
@@ -23,47 +26,57 @@ import java.io.InputStreamReader;
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
 			String line = null;
-			
-			while ((line = br.readLine()) !=null) {
-				String[] record = line.trim().split("0");
-				if (record.length != 2) continue;
+
+			while ((line = br.readLine()) != null) {
+				String[] record = line.trim().split("@");
+				if (record.length != 2)
+					continue;
 				parse(record[0], record[1]);
 			}
-		
+
 			br.close();
-		}catch (Exception e) {
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	private void parse(String text, String lang, int...ks) {
-		Language language = Language.valueOf(lang);
-		
-		for(int i = 0; i <= text.length() - k; i++) {
-			CharSequence kmer = text.substring(i,i + k);
-			db.add(kmer, language);
+
+	public void parse(String text, String lang, int... ks) {
+		Language l = Language.valueOf(lang);
+
+		for (int i = 0; i < text.length() - k; i++) {
+			// get 3-mer
+			CharSequence kmer = text.substring(i, i + k);
+			db.add(kmer, l);
 		}
 	}
-	
-	public static void main(String[] args) throws Throwable {
-		Parser p = new Parser("Wili-2018-small-11750-Edited.txt", 1);
-		
-		Database db = new Database();
-		p.setDb(db);
-		Thread t = new Thread(p);
-		t.start();
-		t.join();
-		
-		db.resize(300);
-		
-		
-		String s = "This is an example of the english language";
-		p.analyseQuery(s);
-	}
 
-	private void analyseQuery(String s) {
-		// TODO Auto-generated method stub
-		
+	public void analyseQuery(String file) throws IOException {
+		int kmers = 0;
+		int freq = 1;
+		String queryFile;
+
+		Map<Integer, LanguageEntry> query = new HashMap<>();
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+
+		while ((queryFile = br.readLine()) != null) {
+
+			for (int i = 0; i <= queryFile.length() - k; i++) {
+				CharSequence sequenceChar = queryFile.substring(i, i + k);
+				kmers = sequenceChar.hashCode();
+			}
+
+			if (query.containsKey(kmers)) {
+				freq += query.get(kmers).getFrequency();
+			}
+
+			LanguageEntry l = new LanguageEntry(kmers, freq);
+			query.put(kmers, l);
+		}
+
+		Language language = db.getLanguage(query);
+		System.out.println(language);
 	}
-		
 }
